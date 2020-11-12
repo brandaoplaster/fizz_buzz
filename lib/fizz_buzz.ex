410cbd717ec187c96807a5f7b39efb4624ce744c
brandaoplaster <brandaoplaster@gmail.com>
2020-11-11 21:33:53 -0300

    Replacing multiple

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -8,8 +8,18 @@ defmodule FizzBuzz do
   defp handle_file_read({:ok, result}) do
     result
     |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&convert_and_evaluate_numbers/1)
   end
   
   defp handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+
+  defp convert_and_evaluate_numbers(valeu) do
+    number = String.to_integer(valeu)
+    evaluate_numbers(number)
+  end
+
+  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzBuzz
+  defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_numbers(number), do: number
 end
