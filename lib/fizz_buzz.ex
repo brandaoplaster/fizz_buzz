43fb5bb40d8402b600a264aca3bcd5679f9c913b
brandaoplaster <brandaoplaster@gmail.com>
2020-11-11 21:52:03 -0300

    Refactoring code

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -6,16 +6,20 @@ defmodule FizzBuzz do
   end
 
   defp handle_file_read({:ok, result}) do
-    result
-    |> String.split(",")
-    |> Enum.map(&convert_and_evaluate_numbers/1)
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate_numbers/1)
+
+    {:ok, result}  
   end
   
-  defp handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
 
-  defp convert_and_evaluate_numbers(valeu) do
-    number = String.to_integer(valeu)
-    evaluate_numbers(number)
+  defp convert_and_evaluate_numbers(value) do
+    value
+    |> String.to_integer()
+    |> evaluate_numbers()
   end
 
   defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzBuzz
